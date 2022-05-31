5161499d4acbc0a174d7cc885d77952c29a07457
gaoleiwinter <gaoleiwinter@gmail.com>
2022-05-31 18:16:33 +0800

    添加历史记录页面

--- app/src/main/java/com/winter/nav_eatwhat/ui/page/HistoryActivity.java
+++ app/src/main/java/com/winter/nav_eatwhat/ui/page/HistoryActivity.java
@@ -2,15 +2,19 @@ package com.winter.nav_eatwhat.ui.page;
 
 import com.kunminx.architecture.ui.page.DataBindingConfig;
 import com.winter.lib_common.ui.page.BaseActivity;
+import com.winter.nav_eatwhat.BR;
+import com.winter.nav_eatwhat.R;
+import com.winter.nav_eatwhat.ui.state.HistoryViewModel;
 
 public class HistoryActivity extends BaseActivity {
+    HistoryViewModel viewModel;
     @Override
     protected void initViewModel() {
-
+        viewModel = getActivityScopeViewModel(HistoryViewModel.class);
     }
 
     @Override
     protected DataBindingConfig getDataBindingConfig() {
-        return null;
+        return new DataBindingConfig(R.layout.activity_history_food, BR.vm, viewModel);
     }
 }
