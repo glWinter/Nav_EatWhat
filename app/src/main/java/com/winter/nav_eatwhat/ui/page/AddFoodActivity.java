797ee7393c63851a2fc43b8c5466e94fe73edbb5
gaoleiwinter <gaoleiwinter@gmail.com>
2022-06-01 11:34:37 +0800

    添加选择照片和裁剪的功能

--- app/src/main/java/com/winter/nav_eatwhat/ui/page/AddFoodActivity.java
+++ app/src/main/java/com/winter/nav_eatwhat/ui/page/AddFoodActivity.java
@@ -2,8 +2,17 @@ package com.winter.nav_eatwhat.ui.page;
 
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
+import androidx.annotation.Nullable;
+
+import com.bumptech.glide.Glide;
 import com.kunminx.architecture.ui.page.DataBindingConfig;
+import com.theartofdev.edmodo.cropper.CropImage;
+import com.theartofdev.edmodo.cropper.CropImageView;
 import com.winter.lib_common.ui.page.BaseActivity;
 import com.winter.nav_eatwhat.BR;
 import com.winter.nav_eatwhat.R;
@@ -20,6 +29,28 @@ public class AddFoodActivity extends BaseActivity {
     @Override
     protected DataBindingConfig getDataBindingConfig() {
         return new DataBindingConfig(R.layout.activity_add_food, BR.vm, viewModel);
+    }
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        findViewById(R.id.btn).setOnClickListener(v -> CropImage.activity()
+                .setGuidelines(CropImageView.Guidelines.ON)
+                .start(AddFoodActivity.this));
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
+            CropImage.ActivityResult result = CropImage.getActivityResult(data);
+            if (resultCode == RESULT_OK&&result!=null) {
+                Uri resultUri = result.getUri();
+                Glide.with(AddFoodActivity.this).load(resultUri).into((ImageView) findViewById(R.id.img));
+            } else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
+                Exception error = result.getError();
+            }
+        }
     }
 
     public static void start(Context context){
